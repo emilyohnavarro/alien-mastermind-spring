b88e7e10276b7761e58894af276437c633654df7
Emily Navarro <emilyo@uci.edu>
2021-11-05 05:53:48 -0700

    First commit (really) - last one was wrong directory

---
+++ src/main/java/alienmastermind/AlienMastermind.java
@@ -0,0 +1,13 @@
+package alienmastermind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienMastermind {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienMastermind.class, args);
+	}
+
+}
