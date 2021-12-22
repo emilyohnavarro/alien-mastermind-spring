c457fd0c4de14f19808da2bbd25cc685a91ca277
Emily Navarro <emilyo@uci.edu>
2021-12-22 05:36:40 -0800

    Added a test

--- src/test/java/alienmastermind/AlienMastermindTests.java
+++ src/test/java/alienmastermind/AlienMastermindTests.java
@@ -1,10 +1,10 @@
-package com.example.servingwebcontent;
+package alienmastermind;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServingWebContentApplicationTests {
+class AlienMastermindTests {
 
 	@Test
 	void contextLoads() {
